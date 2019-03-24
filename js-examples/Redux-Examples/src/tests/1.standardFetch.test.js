59acc48ededa5b1529122c7b7af7ed1f2f3add1d
robcronin <robc@theodo.co.uk>
2019-03-24 17:42:05 +0000

    Add error case

--- js-examples/Redux-Examples/src/tests/1.standardFetch.test.js
+++ js-examples/Redux-Examples/src/tests/1.standardFetch.test.js
@@ -2,16 +2,21 @@ import 'isomorphic-fetch';
 
 import { combineReducers } from 'redux';
 import { push } from 'react-router-redux';
-import { expectSaga, matchers } from 'redux-saga-test-plan';
+import { expectSaga, matchers, providers } from 'redux-saga-test-plan';
 
 import dataSaga from '../saga';
-import { fetchDataRequest, fetchDataSuccess } from '../actions';
+import {
+  fetchDataRequest,
+  fetchDataSuccess,
+  fetchDataFailure,
+} from '../actions';
 import { myReducer, initialState as initialMyReducerState } from '../reducer';
 
 describe('dataSaga', () => {
   const initialState = {
     myReducer: initialMyReducerState,
   };
+
   it('should retrieve data from the server and send a SUCCESS action', () => {
     const finalState = {
       ...initialState,
@@ -41,6 +46,36 @@ describe('dataSaga', () => {
         // Check that expected stuff has happened (order doesn't matter)
         .put(fetchDataSuccess(['wow some api']))
         .put(push('/data-page'))
+        // Check final state
+        .hasFinalState(finalState)
+        .silentRun()
+    );
+  });
+
+  it('should call FAILURE action if the fetch throws an error', () => {
+    const finalState = {
+      ...initialState,
+      myReducer: {
+        ...initialState.myReducer,
+        error: '404 error',
+      },
+    };
+    return (
+      expectSaga(dataSaga)
+        // Setup mock with error
+        .provide([
+          [
+            matchers.call(fetch, 'https://api/endpoint'),
+            providers.throwError('404 error'),
+          ],
+        ])
+        .withReducer(combineReducers({ myReducer }), initialState)
+        .dispatch(fetchDataRequest())
+        .put(fetchDataFailure('404 error'))
+
+        // Check that some stuff does not happen
+        .not.put(push('/data-page'))
+
         // Check final state
         .hasFinalState(finalState)
         .silentRun()
